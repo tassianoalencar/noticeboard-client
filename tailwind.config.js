1bf1e4f7077e3f83447ea1b5ea886d0680fef404
Tassiano Alencar <tassiano.alencar@icloud.com>
2021-02-11 22:18:55 -0300

    Inint

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sans': ['nunito'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
